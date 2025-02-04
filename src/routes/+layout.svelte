4b3f8f785cd9789f0dcb30f6d4874da73832c760
PillowGit <goldentaco727@gmail.com>
2025-02-04 00:55:30 -0800

    Add basic sveltekit project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<title>Linkly</title>
+<div class="min-w-screen flex min-h-screen flex-col items-center justify-center">
+	{@render children()}
+</div>
