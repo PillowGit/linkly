61fdcf9586f69b9cb855d1d49f6400938e2a1fba
PillowGit <goldentaco727@gmail.com>
2025-02-25 14:49:30 -0800

    New (hopefully working) base

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<title>Linkly</title>
+<div
+	class="text-complement bg-dark flex min-h-screen min-w-screen flex-col items-center justify-center"
+>
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Hello from '$lib/components/Hello.svelte';
+</script>
+
+<Hello />
