9fd9bbfef01ea3eb986a955af43a0758ce8ea3e3
PillowGit <goldentaco727@gmail.com>
2025-02-25 14:46:23 -0800

    Rewriting from base with ts in mind

--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import '../app.css';
-	let { children } = $props();
-</script>
-
-<title>Linkly</title>
-<div class="min-w-screen flex min-h-screen flex-col items-center justify-center">
-	{@render children()}
-</div>
