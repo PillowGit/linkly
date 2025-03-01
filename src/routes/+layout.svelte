756d88fc305e20d0661b86a1ab101392f70e590f
PillowGit <goldentaco727@gmail.com>
2025-03-01 01:04:52 -0800

    Login/Homepage done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 
 <title>Linkly</title>
 <div
-	class="bg-bg dark:bg-bgd text-fg dark:text-fgd flex min-h-screen min-w-screen flex-col items-center justify-center"
+	class="bg-bg dark:bg-bgd text-fg dark:text-fgd flex min-h-screen min-w-screen flex-col items-center justify-center font-light"
 >
 	{@render children()}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,41 @@
 <script lang="ts">
 	import { SignIn, SignOut } from '@auth/sveltekit/components';
 	import { page } from '$app/state';
-
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		console.dir(page.data.session, { depth: null });
-	});
 </script>
 
-{#if page.data.session}
-	{#if page.data.session.user?.image}
-		<img src={page.data.session.user.image} class="avatar" alt="User Avatar" />
-	{/if}
-	<span class="signedInText">
-		<small>Signed in as</small><br />
-		<strong>{page.data.session.user?.name ?? 'User'}</strong>
-	</span>
-	<SignOut>
-		<div slot="submitButton" class="buttonPrimary">Sign out</div>
-	</SignOut>
-{:else}
-	<span class="notSignedInText">You are not signed in</span>
-	<SignIn provider="google">
-		<div slot="submitButton" class="buttonPrimary">Sign in</div>
-	</SignIn>
-{/if}
+<div class="hidden md:block">
+	<div class="flex items-center justify-between">
+		<img src="/icons/linkly_filled.svg" alt="Linkly Mascot" class="mr-16 max-h-[40vh] min-h-64" />
+		<div class="flex flex-col items-start justify-between">
+			<h1 class="text-complement text-6xl font-medium">Linkly</h1>
+			<p class="text-complement mt-6 text-2xl">Shorten. Save. Share. Simplify with Linkly.</p>
+			{#if page.data.session}
+				<SignOut>
+					<div
+						slot="submitButton"
+						class="border-complement text-complement mt-6 flex cursor-pointer justify-center rounded-md border-2 p-2 transition hover:scale-105"
+						role="button"
+						aria-pressed="false"
+						tabindex="0"
+					>
+						Log Out
+					</div>
+				</SignOut>
+			{:else}
+				<SignIn provider="google">
+					<div
+						slot="submitButton"
+						class="border-complement mt-6 flex cursor-pointer justify-center rounded-md border-2 p-2 transition hover:scale-105"
+						role="button"
+						aria-pressed="false"
+						tabindex="0"
+					>
+						<img src="/icons/google.svg" alt="Google Logo" class="h-6 w-6" />
+						<p class="text-complement ml-2">Log in with Google</p>
+					</div>
+				</SignIn>
+			{/if}
+		</div>
+	</div>
+</div>
+<div class="block md:hidden">Mobile not done yet D:</div>
