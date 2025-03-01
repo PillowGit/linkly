741902b57e7a2c5e1cf4d6c3d2b75a4d6e0bc02e
PillowGit <goldentaco727@gmail.com>
2025-03-01 13:30:16 -0800

    Expose cookies to page.data.cookies

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,14 @@
 import type { LayoutServerLoad } from './$types';
 
+const excluded_cookies = ['next-auth.csrf-token', 'next-auth.callback-url', 'authjs.callback-url'];
+
 export const load: LayoutServerLoad = async (event) => {
 	return {
-		session: await event.locals.auth()
+		session: await event.locals.auth(),
+		cookies: event.cookies
+			.getAll()
+			.filter((cookie) => !excluded_cookies.includes(cookie.name))
+			.reduce((obj, item) => Object.assign(obj, { [item.name]: item.value }), {})
 	};
 };
 
