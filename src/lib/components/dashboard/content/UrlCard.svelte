d6d7f55253db35f4552417a1d1f21b58f416f3fa
PillowGit <goldentaco727@gmail.com>
2025-03-04 23:05:04 -0800

    Add redirects to cards

--- src/lib/components/dashboard/content/UrlCard.svelte
+++ src/lib/components/dashboard/content/UrlCard.svelte
@@ -1,18 +1,35 @@
 <script lang="ts">
 	import type { Url } from '$lib/types';
 	let { urlData, ...others } = $props();
-	urlData as Url;
+
+	function CopyURL(e: KeyboardEvent | MouseEvent) {
+		if (e instanceof KeyboardEvent && e.key !== 'Enter') {
+			return;
+		}
+		navigator.clipboard.writeText(`https://linkly.sh/${urlData.id}`);
+	}
 </script>
 
 <div
 	class="border-transparent-mixed bg-bg text-fg mx-4 flex h-full min-w-72 flex-col items-start justify-between rounded-lg border-2"
 >
-	<!-- Link Icon -->
-	<img src="/icons/link_icon.svg" alt="Link Icon" class="mt-4 ml-4 aspect-square h-8" />
+	<!-- Link Icons -->
+	<div class="mt-4 flex h-8 w-full items-center justify-between">
+		<img src="/icons/link_icon.svg" alt="Link Icon" class="ml-4 aspect-square h-8" />
+		<div role="button" aria-pressed="false" tabindex="0" onclick={CopyURL} onkeydown={CopyURL}>
+			<img
+				src="/icons/copy_icon.svg"
+				alt="Copy Icon"
+				class="mr-4 aspect-square h-8 transition hover:scale-110 hover:cursor-pointer hover:invert-25"
+			/>
+		</div>
+	</div>
 	<!-- Link Title/URL -->
 	<div class="ml-4 flex w-full flex-col items-start justify-center">
 		<div class="text-lg font-bold"><h4>{urlData.title}</h4></div>
-		<div class="text-sm"><p>linkly.sh/{urlData.id}</p></div>
+		<a href={`https://linkly.sh/${urlData.id}`} target="_blank" rel="noopener noreferrer"
+			><div class="text-sm underline"><p>linkly.sh/{urlData.id}</p></div></a
+		>
 	</div>
 	<!-- Buttons -->
 	<div class="mx-4 mb-4 flex h-6 w-[90%] items-center justify-between">
