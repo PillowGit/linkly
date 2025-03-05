08889ceae15da9d36688fe505a74f72d1bb03084
PillowGit <goldentaco727@gmail.com>
2025-03-05 13:25:30 -0800

    Prepare actions for link management

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -66,6 +66,90 @@ export const actions: Actions = {
 			return { error: `An error occured: ${error.message}` };
 		}
 
-	  throw redirect(303, '/dashboard');
+		throw redirect(303, '/dashboard');
+	},
+	update: async (event) => {
+		// Get form data
+		const { request } = event;
+		const session = await event.locals.auth();
+		if (!session?.user) throw redirect(303, '/');
+		const formData = await request.formData();
+		const longUrl = formData.get('longUrl') as string;
+
+		// Verify URL was given
+		if (!longUrl) {
+			return { error: 'Please enter a URL.' };
+		}
+
+		// Check that it's a valid URL
+		try {
+			new URL(longUrl);
+		} catch {
+			return { error: 'Please enter a valid URL.' };
+		}
+
+		// Get a new url from the database
+		const shortUrl = await generateNewUrl(6);
+
+		const url_title = longUrl.split('://')[1].split('/')[0];
+
+		// Insert the new URL into the database
+		const { error } = await supabase.from('redirects').insert({
+			id: shortUrl,
+			destination: longUrl,
+			owner_email: session.user.email,
+			created_on: new Date().toISOString(),
+			title: url_title,
+			total_visits: 0
+		});
+
+		if (error) {
+			console.error(error);
+			return { error: `An error occured: ${error.message}` };
+		}
+
+		throw redirect(303, '/dashboard');
+	},
+	delete: async (event) => {
+		// Get form data
+		const { request } = event;
+		const session = await event.locals.auth();
+		if (!session?.user) throw redirect(303, '/');
+		const formData = await request.formData();
+		const longUrl = formData.get('longUrl') as string;
+
+		// Verify URL was given
+		if (!longUrl) {
+			return { error: 'Please enter a URL.' };
+		}
+
+		// Check that it's a valid URL
+		try {
+			new URL(longUrl);
+		} catch {
+			return { error: 'Please enter a valid URL.' };
+		}
+
+		// Get a new url from the database
+		const shortUrl = await generateNewUrl(6);
+
+		const url_title = longUrl.split('://')[1].split('/')[0];
+
+		// Insert the new URL into the database
+		const { error } = await supabase.from('redirects').insert({
+			id: shortUrl,
+			destination: longUrl,
+			owner_email: session.user.email,
+			created_on: new Date().toISOString(),
+			title: url_title,
+			total_visits: 0
+		});
+
+		if (error) {
+			console.error(error);
+			return { error: `An error occured: ${error.message}` };
+		}
+
+		throw redirect(303, '/dashboard');
 	}
 };
