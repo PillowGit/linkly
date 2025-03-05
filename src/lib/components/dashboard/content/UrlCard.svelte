a5bc10957aa538e7b1a5339f99474b32a7096987
PillowGit <goldentaco727@gmail.com>
2025-03-05 13:25:39 -0800

    Add interactables to cards

--- src/lib/components/dashboard/content/UrlCard.svelte
+++ src/lib/components/dashboard/content/UrlCard.svelte
@@ -8,6 +8,12 @@
 		}
 		navigator.clipboard.writeText(`https://linkly.sh/${urlData.id}`);
 	}
+	function Unimplemented(e: KeyboardEvent | MouseEvent) {
+		if (e instanceof KeyboardEvent && e.key !== 'Enter') {
+			return;
+		}
+		alert('This feature is not implemented yet');
+	}
 </script>
 
 <div
@@ -16,12 +22,15 @@
 	<!-- Link Icons -->
 	<div class="mt-4 flex h-8 w-full items-center justify-between">
 		<img src="/icons/link_icon.svg" alt="Link Icon" class="ml-4 aspect-square h-8" />
-		<div role="button" aria-pressed="false" tabindex="0" onclick={CopyURL} onkeydown={CopyURL}>
-			<img
-				src="/icons/copy_icon.svg"
-				alt="Copy Icon"
-				class="mr-4 aspect-square h-8 transition hover:scale-110 hover:cursor-pointer hover:invert-25"
-			/>
+		<div
+			role="button"
+			class="mr-4 transition hover:scale-110 hover:cursor-pointer hover:invert-25 focus:scale-110 focus:invert-25"
+			aria-pressed="false"
+			tabindex="0"
+			onclick={CopyURL}
+			onkeydown={CopyURL}
+		>
+			<img src="/icons/copy_icon.svg" alt="Copy Icon" class="aspect-square h-8" />
 		</div>
 	</div>
 	<!-- Link Title/URL -->
@@ -35,7 +44,12 @@
 	<div class="mx-4 mb-4 flex h-6 w-[90%] items-center justify-between">
 		<!-- View Analytics -->
 		<div
-			class="mr-1 flex h-full items-center justify-center text-xs transition hover:scale-105 hover:cursor-pointer"
+			class="mr-1 flex h-full items-center justify-center text-xs transition hover:scale-105 hover:cursor-pointer hover:font-medium focus:scale-105 focus:font-medium"
+			role="button"
+			aria-pressed="false"
+			tabindex="0"
+			onclick={Unimplemented}
+			onkeydown={Unimplemented}
 		>
 			<img
 				src="/icons/small_analytics_icon.svg"
@@ -48,7 +62,12 @@
 		<div class="border-transparent-mixed mx-2 h-full border-[0.5px]"></div>
 		<!-- Edit -->
 		<div
-			class="text-complement2 mr-1 flex h-full items-center justify-center text-xs transition hover:scale-105 hover:cursor-pointer"
+			class="text-complement2 mr-1 flex h-full items-center justify-center text-xs transition hover:scale-105 hover:cursor-pointer hover:font-medium focus:scale-105 focus:font-medium"
+			role="button"
+			aria-pressed="false"
+			tabindex="0"
+			onclick={Unimplemented}
+			onkeydown={Unimplemented}
 		>
 			<img src="/icons/small_edit_icon.svg" alt="Edit Icon" class="mr-1 aspect-square h-[80%]" />
 			<p>Edit</p>
@@ -56,10 +75,15 @@
 		<!-- Spacer -->
 		<div class="border-transparent-mixed mx-2 h-full border-[0.5px]"></div>
 		<!-- Trash/Delete -->
-		<img
-			src="/icons/small_trash_icon.svg"
-			alt="Trash Icon"
-			class="mr-1 aspect-square h-[80%] transition hover:scale-110 hover:cursor-pointer"
-		/>
+		<div
+			class="mr-1 transition hover:scale-110 hover:cursor-pointer focus:scale-110"
+			role="button"
+			aria-pressed="false"
+			tabindex="0"
+			onclick={Unimplemented}
+			onkeydown={Unimplemented}
+		>
+			<img src="/icons/small_trash_icon.svg" alt="Trash Icon" class="aspect-square h-[80%]" />
+		</div>
 	</div>
 </div>
